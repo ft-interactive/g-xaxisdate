3cb964c425db13c075a95ccc54dec667040a5a73
Bob Haslett <bob.haslett@ft.com>
2017-05-31 17:21:05 +0100

    Drop in code from x-axis function

--- test/xaxisDate-test.js
+++ test/xaxisDate-test.js
@@ -2,6 +2,6 @@ var tape = require("tape"),
     axis = require("../");
 
 tape("Should return 42", function(test) {
-  test.equal(axis.xaxisDate().interval(), 0);
+  test.equal(axis.xaxisDate().rem(), 10);
   test.end();
 });
